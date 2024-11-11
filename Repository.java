79cd7b6ad3dc79822d24342943f7fd8920923302
Pokiwar01 <nightsilverz8@gmail.com>
2024-11-11 15:28:21 +0700

    Delete Repository.java

--- Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employee;
-
-import com.example.employee.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface UserRepository extends JpaRepository<User, Long> {
-}
